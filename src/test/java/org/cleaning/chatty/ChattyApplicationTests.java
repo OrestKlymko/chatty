4241750d164019aa216633166cbbf5513537a093
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2024-07-01 11:45:37 +0200

    Chatty - Create configuration ws

---
+++ src/test/java/org/cleaning/chatty/ChattyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cleaning.chatty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
