e092c4c0d691082d253dbe8306c95f5636cb961f
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2024-07-01 12:20:15 +0200

    Chatty - Add event listener

--- src/main/java/org/cleaning/chatty/config/WebSocketEventListener.java
+++ src/main/java/org/cleaning/chatty/config/WebSocketEventListener.java
@@ -2,7 +2,11 @@ package org.cleaning.chatty.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.cleaning.chatty.model.ChatMessage;
+import org.cleaning.chatty.model.MessageType;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,8 +15,24 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+	private final SimpMessageSendingOperations messageOperations;
 	@EventListener
-	public void disconnectListener(SessionDisconnectEvent event){
+	public void disconnectListener(SessionDisconnectEvent event) {
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+		String username = "";
+		if (accessor.getSessionAttributes() != null &&
+				accessor.getSessionAttributes().containsKey("username")) {
+			username = (String) accessor.getSessionAttributes().get("username");
+		}
+		if(username.isEmpty()){
+			log.info("User disconnected: {}",username);
+
+			ChatMessage chatMessage = ChatMessage.builder()
+					.messageType(MessageType.LEAVE)
+					.sender(username)
+					.build();
+			messageOperations.convertAndSend("/topic/public",chatMessage);
+		}
 
 	}
 }
